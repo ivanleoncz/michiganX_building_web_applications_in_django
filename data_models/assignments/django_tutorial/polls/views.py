ebc9cd8cfb4c320d520329c3fc14916078c54b6d
ivanleoncz <ivanlmj@gmail.com>
2021-03-21 21:55:05 -0600

    Improving results and vote views
    - results/ displays the poll results, per choice
    - vote/ allows voting for a poll choice and presents the poll results,
    also presenting the poll choices again, in case that vote button is
    pressed without selecting an option

--- data_models/assignments/django_tutorial/polls/views.py
+++ data_models/assignments/django_tutorial/polls/views.py
@@ -1,5 +1,6 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
 
 from .models import Question
 
@@ -22,8 +23,22 @@ def detail(request, question_id):
 
 
 def results(request, question_id):
-    return HttpResponse(f"You're looking at the results of question {question_id}")
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
 
 
 def vote(request, question_id):
-    return HttpResponse(f"You're voting on question {question_id}")
+    question = get_object_or_404(Question, pk=question_id)
+    choice = request.POST.get('choice')
+
+    if question and choice:
+        selected_choice = question.choice_set.get(pk=choice)
+        selected_choice.votes +=1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+    else:
+        return render(request, 'polls/detail.html',
+                {
+                    'question': question,
+                    'error_message': "You didn't select a choice."
+                })
